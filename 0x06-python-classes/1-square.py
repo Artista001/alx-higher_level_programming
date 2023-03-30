09992b71d1b06a4b83e6f4c23a086e2d1f387ee8
Artista001 <samwuels003@gmail.com>
2023-03-30 01:54:35 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Authored by Samwuel simiyu
+"""We define our square"""
+
+
+class Square:
+   """Represents our square"""
+
+   def __init__(self, size):
+       """Private instance attribute: size
+          Instantination with size(no type/value verification)"""
+       self.__size = size
