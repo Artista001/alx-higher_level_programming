27bf65ecc132f5b5cd4d542e574b12e6268cf950
Artista001 <samwuels003@gmail.com>
2023-05-17 19:30:13 -0700

    databases

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- script that creates database hbtn_0c_0 in MySQL server
+-- The script does not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- lists all the tables of database in MySQL Server
+-- The database name will be passed as argument of mysql command
+SHOW TABLES;
