561c2b0cfcc07b304c67d2a0e6a0bf9e7b6a275a
Tech Geek <107541026+Artista001@users.noreply.github.com>
2023-05-20 01:39:50 +0300

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- inserts a new row in the table first_table (database hbtn_0c_0)
--- database will be passed as the argument
-INSERT INTO first_table(id, name) VALUES (89, 'BestSchool);
+-- inserts a new row in table first_table
+-- in database hbtn_0c_0
+INSERT INTO first_table(id, name) VALUES (89, 'Best School');
